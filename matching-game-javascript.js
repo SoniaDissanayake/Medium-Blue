8c0d021e6583d2eeba14d8af94c708fb6627f48d
Alex Yao <alexyao@Alexs-MacBook-Pro.local>
2021-06-29 11:54:15 -0700

    Added a little bit of basic setup to the .js file

--- matching-game-javascript.js
+++ matching-game-javascript.js
@@ -1,2 +1,13 @@
+// Global Variables
+var cards = [];         // store all the cards
+var turnCounter = 0;    // the number of turns
 
 
+// Functions
+gameSetup()
+
+// The function that will start/reset the game back to its original state
+function gameSetup()
+{
+
+}
