bc22684239b4b8d1fd12eef5e322651f2b761968
hsmith2727 <hschieffelinsmith@gmail.com>
2021-06-29 15:48:04 -0400

    Shuffle Cards

--- matching-game-javascript.js
+++ matching-game-javascript.js
@@ -1,5 +1,5 @@
 // function for creating cards
-function gamesetup() {
+function cardCreation() {
     var cards = []
     for (i=1; i<9; i++) {
         var card1 = {
@@ -13,6 +13,16 @@ function gamesetup() {
 }
 
 function shuffle() {
-    var cards = gamesetup()
-    
+    var cards = cardCreation();
+    var cardsMoved = 0;
+    for (i=0; i<100; i++) {
+        var selection1 = Math.floor(Math.random() * 16);
+        var selection2 = Math.floor(Math.random() * 16);
+        var temp = cards[selection1];
+        cards[selection1] = cards[selection2];
+        cards[selection2] = temp;
+    }
+    console.log(cards) 
 }
+
+shuffle()
