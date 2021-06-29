690cc17799382c99c44984917c8409d3132f1c3b
hsmith2727 <hschieffelinsmith@gmail.com>
2021-06-29 15:16:10 -0400

    Added gameset and started shuffle

--- matching-game-javascript.js
+++ matching-game-javascript.js
@@ -1,2 +1,18 @@
+// function for creating cards
+function gamesetup() {
+    var cards = []
+    for (i=1; i<9; i++) {
+        var card1 = {
+            value: i,
+            faceUp: false,
+        }
+        cards.push(card1)
+        cards.push(card1)
+    }
+    return cards
+}
 
-
+function shuffle() {
+    var cards = gamesetup()
+    
+}
