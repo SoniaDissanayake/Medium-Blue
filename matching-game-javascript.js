e0bc59783622c9428f425135d680e5d0b7d33591
hsmith2727 <hschieffelinsmith@gmail.com>
2021-06-29 15:53:13 -0400

    test

--- matching-game-javascript.js
+++ matching-game-javascript.js
@@ -1,17 +1,3 @@
-// function for creating cards
-function cardCreation() {
-    var cards = []
-    for (i=1; i<9; i++) {
-        var card1 = {
-            value: i,
-            faceUp: false,
-        }
-        cards.push(card1)
-        cards.push(card1)
-    }
-    return cards
-}
-
 function shuffle() {
     var cards = cardCreation();
     var cardsMoved = 0;
